48c0258033b36f72adf7068fa33382219263c0f1
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-06-30 14:25:13 +0530

    Rename Cloud Runner to Orchestrator (#775)
    * Rename "Cloud Runner" to "Orchestrator" across entire codebase
    
    Breaking change: All CloudRunner classes, options, environment variables,
    and action.yml inputs have been renamed to Orchestrator equivalents.
    
    - Renamed src/model/cloud-runner/ directory to src/model/orchestrator/
    - Renamed all cloud-runner-* files to orchestrator-*
    - Renamed all CloudRunner* classes to Orchestrator* (15+ classes)
    - Renamed all cloudRunner* properties to orchestrator* equivalents
    - Renamed CLOUD_RUNNER_* env vars to ORCHESTRATOR_*
    - Updated action.yml [CloudRunner] markers to [Orchestrator]
    - Updated workflow files and package.json test scripts
    - Updated all runtime strings (cache paths, log messages, branch refs)
    - Rebuilt dist/index.js
    
    No backward compatibility layer is provided.
    
    * Remove tracked log/temp files and add to .gitignore
    
    Remove $LOG_FILE and temp/job-log.txt debug artifacts that should
    not be in the repository.
    
    ---------

--- src/model/orchestrator/options/orchestrator-environment-variable.ts
+++ src/model/orchestrator/options/orchestrator-environment-variable.ts
@@ -3,3 +3,8 @@ class OrchestratorEnvironmentVariable {
   public value!: string;
 }
 export default OrchestratorEnvironmentVariable;
+class OrchestratorEnvironmentVariable {
+  public name!: string;
+  public value!: string;
+}
+export default OrchestratorEnvironmentVariable;
